d747939a4fbccb8d572f86b1b684299541f027e7
matkuh <matkuh@gmail.com>
2019-07-22 16:56:08 -0700

    add cdns and nav bar to main handlebars, add handlebar requests to server.js

--- server.js
+++ server.js
@@ -25,69 +25,88 @@ app.set("view engine", "handlebars");
 
 mongoose.connect("", { useNewUrlParser: true });
 
+app.get("/", function (req, res) {
+    Article.find({ "saved": false }, function (error, data) {
+        var result = {
+            article: data
+        };
+        console.log(result);
+        res.render("index", result);
+    });
+});
 
-app.get("/scrape", function(req, res) {
-  axios.get("https://old.reddit.com/").then(function(response) {
-    var $ = cheerio.load(response.data);
+app.get("/saved", function (req, res) {
+    Article.find({ "saved": true }).populate("notes").exec(function (error, articles) {
+        var result = {
+            article: articles
+        };
+        res.render("saved", result);
+    });
+});
 
-    $("article").each(function(i, element) {
-      var result = {};
 
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
+app.get("/scrape", function (req, res) {
+    axios.get("https://old.reddit.com/").then(function (response) {
+        var $ = cheerio.load(response.data);
 
-      db.Article.create(result)
-        .then(function(dbArticle) {
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-          console.log(err);
+        $("article").each(function (i, element) {
+            var result = {};
+
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
         });
-    });
 
-    res.send("Scrape Complete");
-  });
-});
-
-app.get("/articles", function(req, res) {
-  db.Article.find({})
-    .then(function(dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      res.json(err);
+        res.send("Scrape Complete");
     });
 });
 
-app.get("/articles/:id", function(req, res) {
-  db.Article.findOne({ _id: req.params.id })
-    .populate("note")
-    .then(function(dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
+app.get("/articles", function (req, res) {
+    db.Article.find({})
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
 });
 
-app.post("/articles/:id", function(req, res) {
-  db.Note.create(req.body)
-    .then(function(dbNote) {
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-    })
-    .then(function(dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
+app.get("/articles/:id", function (req, res) {
+    db.Article.findOne({ _id: req.params.id })
+        .populate("note")
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
+
+app.post("/articles/:id", function (req, res) {
+    db.Note.create(req.body)
+        .then(function (dbNote) {
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+        })
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
 });
 
 
-app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
 });
