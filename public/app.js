0fa1757d89a38f8abb4054a5edfdc7ab3a312e37
matkuh <matkuh@gmail.com>
2019-07-23 16:26:24 -0700

    fix note modal

--- public/app.js
+++ public/app.js
@@ -1,56 +1,65 @@
-$("#scrape").on("click", function(){
+$("#scrape").on("click", function () {
     $.ajax({
         method: "GET",
         url: "/scrape",
-    }).done(function(data){
+    }).done(function (data) {
         console.log(data)
         window.location = "/"
     })
 })
 
-$(".save").on("click", function(){
-    var articleId = $(this).attr("data-id");
+$(".save").on("click", function () {
+    var articlesId = $(this).attr("data-id");
     $.ajax({
         method: "Post",
-        url: "/articles/save/" + articleId
-    }).done(function(data){
+        url: "/articles/save/" + articlesId
+    }).done(function (data) {
         window.location = "/"
     })
 })
 
-$(".delete").on("click", function(){
-    var articleId = $(this).attr("data-id");
+$(".delete").on("click", function () {
+    var articlesId = $(this).attr("data-id");
     $.ajax({
         method: "Post",
-        url: "/articles/delete/" + articleId
-    }).done(function(data){
+        url: "/articles/delete/" + articlesId
+    }).done(function (data) {
         window.location = "/saved"
     })
 })
 
-$(".saveNote").on("click", function(){
-    var articleId = $(this).attr("data-id");
-    $.ajax({
-        method: "POST",
-        url: "/notes/save/" + articleId,
-        data: {
-            text: $("#noteBody" + articleId).val()
-        }
-    }).done(function(data){
-        $("noteBody" + articleId).val("")
-        $(".modalNote").modal("hide");
-        window.location = "/saved"
-    })
+$(".saveNote").on("click", function () {
+    var articlesId = $(this).attr("data-id");
+    if (!$("#noteBody" + articlesId).val()) {
+        alert("please enter a note to save")
+        $.ajax({
+            method: "POST",
+            url: "/notes/save/" + articlesId,
+            data: {
+                text: $("#noteBody" + articlesId).val()
+            }
+        }).done(function (data) {
+            $("#noteBody" + articlesId).val("")
+            $(".modalNote").modal("close");
+            window.location = "/saved"
+        })
+    }
 })
 
-$(".deleteNote").on("click", function(){
+$(".deleteNote").on("click", function () {
     var noteId = $(this).attr("data-note-id")
     var articleId = $(this).attr("data-article-id")
     $.ajax({
         method: "DELETE",
         url: "/notes/delete/" + noteId + "/" + articleId
-    }).done(function(data){
+    }).done(function (data) {
         $(".modalNote").modal("hide")
         window.location = "/saved"
     })
+})
+
+$(".addNote").on("click", function () {
+    $('.modal').modal();
+
+    $('.trigger-modal').modal()
 })
